9d88814e2dba67e1095f53a9e97658b562e21c06
Vasanthy Kolluri <vasanthy@yahoo-inc.com>
2014-06-15 22:58:56 -0700

    Added a draft version of Filter activity

---
+++ src/com/example/gridimagesearch/FilterActivity.java
@@ -0,0 +1,30 @@
+package com.example.gridimagesearch;
+
+import android.app.Activity;
+import android.app.ActionBar;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+
+public class FilterActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_filter);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.filter, menu);
+		return true;
+	}
+
+}
